cbc3368a363f6d4c6ec4e681a64a36c672a2c826
Gael Vivas <gaelvn2020@outlook.com>
2024-10-22 11:45:47 -0600

    poo focus on server and client

--- client.py
+++ client.py
@@ -1,18 +1,31 @@
 import socket
 
+class Client:
+    def __init__(self, host='127.0.0.1', port=9999):
+        self.host = host
+        self.port = port
+        self.client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    def connect(self):
+        self.client_sock.connect((self.host, self.port))
+        print('Conectado al servidor...')
+
+    def send_data(self, data):
+        self.client_sock.send(data.encode())
+
+    def receive_data(self):
+        response = self.client_sock.recv(1024)
+        print('Resultados:', response.decode())
+    
+    def close(self):
+        self.client_sock.close()
+
+
 def main():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(('127.0.0.1',9999))
-    print('Conectado al servidor...')
-
-    # Enviar datos al servidor
-    client.send('10'.encode())
-
-    # Recibir los resultados del servidor
-    response = client.recv(1024)
-    print('Resultados:', response.decode())
-
-    # Cerrar la conexión con el servidor
+    client = Client()
+    client.connect()
+    client.send_data('10')
+    client.receive_data()
     client.close()
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,40 +1,49 @@
-import multiprocessing
 import socket
+import threading
+import multiprocessing
 
-# Tarea específica
-def tarea(num):
-    return num * num
+class Server:
+    def __init__(self, host='127.0.0.1', port=9999):
+        self.host = host
+        self.port = port
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def handle_client(client_socket):
-    # Recibir datos del cliente
-    data = client_socket.recv(1024)
-    if not data:
-        return
-    # Convertir los datos recibidos a un rango de números
-    num_range = range(int(data.decode()))
+    def tarea(self, num):
+        return num * num
 
-    # Crear un grupo de procesos con 4 procesos
-    with multiprocessing.Pool(processes=4) as pool:
-        # Ejecutar la tarea en paralelo de manera asíncrona
-        async_result = pool.map_async(tarea, num_range)
-        print('Esperando a que se completen las tareas...')
-        # Obtener los resultados
-        resultados = async_result.get()
-    
-    # Enviar los resultados al cliente
-    client_socket.send(str(resultados).encode())
-    # Cerrar la conexión con el cliente
-    client_socket.close()
+    def handle_client(self, client_socket):
+        try:
+            data = client_socket.recv(1024)
+            if not data:
+                return
+
+            num_range = range(int(data.decode()))
+
+            with multiprocessing.Pool(processes=4) as pool:
+                async_result = pool.map_async(self.tarea, num_range)
+                resultados = async_result.get()
+
+            client_socket.send(str(resultados).encode())
+        finally:
+            client_socket.close()
+
+    def start(self):
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen(5)
+        print(f'Servidor escuchando en el puerto {self.port}...')
+
+        while True:
+            client_socket, addr = self.server_socket.accept()
+            print(f'Conexión aceptada desde {addr}')
+            client_thread = threading.Thread(target=self.handle_client, args=(client_socket,))
+            client_thread.start()
 
 def main():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('127.0.0.1',9999))
-    print('Servidor escuchando en el puerto 9999...')
-    while True:
-        server.listen(5)
-        client, addr = server.accept()
-        print(f'Conexión aceptada desde {addr}')
-        handle_client(client)
+    server = Server()
+    server.start()
 
 if __name__ == '__main__':
     main()
+
+
+
